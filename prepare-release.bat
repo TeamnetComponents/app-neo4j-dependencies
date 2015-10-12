b7bf6f271a94035138bb7950e8fe71f63becad00
Oana.Mihai <oana.mihai@teamnet.ro>
2015-10-12 15:49:12 +0300

    skip tests

--- prepare-release.bat
+++ prepare-release.bat
@@ -3,10 +3,10 @@ call mvn release:prepare
 call mvn release:perform
 call mvn clean install
 cd ../app-neo4j-base
-call mvn release:prepare
+call mvn release:prepare -Darguments="-DskipTests"
 call mvn release:perform
 cd ../app-ou-java
-call mvn release:prepare
+call mvn release:prepare -Darguments="-DskipTests"
 call mvn release:perform
 cd ../app-ou-ui
 call mvn release:prepare
